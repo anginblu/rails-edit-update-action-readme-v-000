f6b971a852bca862f410bbbc499d166328bb1693
Jacqueline Lee <anginblu@gmail.com>
2018-01-23 21:52:59 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,7 @@ class PostsController < ApplicationController
 
 	def update
 		# raise params.inspect
+		@post = Post.find(params[:id])
 		@post.update(title: params[:title], description: params[:description])
 		redirect_to post_path(@post)
 	end
