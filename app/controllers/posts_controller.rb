b4e48d0ab8d1afd3ac0b9e381a27cc7c16a7bd7e
Jacqueline Lee <anginblu@gmail.com>
2018-01-23 21:49:11 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
 
 	def edit
 		@post = Post.find(params[:id])
-		@post.update()
+		@post.update(title: params[:title], description: params[:description])
 	end
 
 	def update
